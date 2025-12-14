369398369019ba691c794ce7397e56298b9d90ca
oliviamelia <oliviamelia@gmail.com>
2025-12-14 12:11:20 +0800

    report

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { LaporanModule } from './laporan/laporan.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { PrismaModule } from './prisma/prisma.module';
       isGlobal: true,
     }),
     PrismaModule,
+    LaporanModule,
   ],
   controllers: [AppController],
   providers: [AppService],
